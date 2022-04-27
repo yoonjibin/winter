d65415bff480f19dc3dc31d2bb21bade47b4bf83
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-04-27 23:02:44 +0900

    :sparkles: Param을 이용하여 데이터 바꿔보기

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,6 +12,12 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","진시윤 병신");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 
 }
