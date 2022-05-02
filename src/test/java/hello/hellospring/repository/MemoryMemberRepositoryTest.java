4eded9950f7f9ae78600e5e5b477f05f66a84a12
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-02 16:26:43 +0900

    :sparkles: save부분 test코드

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,22 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemberRepository repository=new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member=new Member();
+        member.setName("spring");
+
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+}
