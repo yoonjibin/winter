912f367a5ced505a776fba2cffa39cfb5e5d04eb
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-03 10:14:03 +0900

    :sparkles: 회원가입서비스 및 중복검사

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,24 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+}
