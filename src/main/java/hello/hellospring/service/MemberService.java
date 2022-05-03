63277e185adf297b7420f901016f5fc71e86fcf2
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-03 10:29:42 +0900

    :sparkles: 전체회원 조회 및 유저아이디로 유저찾기

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -24,5 +25,11 @@ public class MemberService {
         memberRepository.findByName(member.getName()).ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
+    }
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Optional<Member>findOne(Long memberId){
+        return memberRepository.findById(memberId)
     }
 }
