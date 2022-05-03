7021cf5afd17fc05d685a5aab2067523feff48b7
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-03 10:20:07 +0900

    :sparkles: Refactoring 기능

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -14,11 +14,15 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        memberRepository.findByName(member.getName()).ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+        validateDuplicateMember(member);//중복회원 검증
 
         memberRepository.save(member);
         return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
     }
 }
