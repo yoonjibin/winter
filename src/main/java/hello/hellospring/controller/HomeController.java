c166fd3394296c3e7c18397265d6f4a9a5add508
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-08 14:03:44 +0900

    :sparkles: mvc예제:멤버 확인

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -3,8 +3,11 @@ package hello.hellospring.controller;
 import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -33,5 +36,11 @@ public class HomeController {
         memberService.join (member);
 
         return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members=memberService.findMembers();
+        model.addAttribute("members",members);
+        return "member/memberList";
     }
 }
