28ae7eb6fae4a321c289f5f215f434bccb183d1d
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2022-05-08 13:55:19 +0900

    :sparkles: mvc예제:멤버 등록

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,12 +1,37 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+
+    private MemberService memberService;
+
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "member/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member ();
+        member.setName (form.getName ());
+
+        memberService.join (member);
+
+        return "redirect:/";
     }
 }
